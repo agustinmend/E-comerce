320d7f9254ba6bb0405408d8b780b46a931a0d1e
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-05-22 00:03:31 -0400

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CatalogoComponent } from '../components/catalogo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CatalogoComponent},
+    { path: '**', redirectTo: ''}
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production : true,
+    supabaseUrl: 'https://gxvyjfwjvhrqfvdoafny.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4dnlqZndqdmhycWZ2ZG9hZm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MzE2OTMsImV4cCI6MjA4NzIwNzY5M30.TqW5RKZIo4rTh0kB9nISyap64FKi3tJjGlWs23Sm9dM'
+};
